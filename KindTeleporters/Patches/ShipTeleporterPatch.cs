cec54392248e282e3827a6975c575e3ffe6b6bd7
stormytuna <stormytuna@outlook.com>
2023-11-28 17:27:26 +0000

    Add project files.

---
+++ KindTeleporters/Patches/ShipTeleporterPatch.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace KindTeleporters.Patches
+{
+    [HarmonyPatch(typeof(ShipTeleporter))]
+    public class ShipTeleporterPatch
+    {
+        private static readonly CodeMatch[] inverseTeleporterPatchIlMatch = new CodeMatch[] {
+            new CodeMatch(i => i.IsLdloc()),
+            new CodeMatch(i => i.LoadsConstant(1)),
+            new CodeMatch(i => i.LoadsConstant(0)),
+            new CodeMatch(i => i.Calls(dropAllHeldItemsMethodInfo))
+        };
+        private static readonly CodeMatch[] teleporterPatchIlMatch = new CodeMatch[] {
+            new CodeMatch(i => i.IsLdarg(0)),
+            new CodeMatch(i => i.opcode == OpCodes.Ldfld), // Cba fetching the field info of the field loaded /shrug
+            new CodeMatch(i => i.LoadsConstant(1)),
+            new CodeMatch(i => i.LoadsConstant(0)),
+            new CodeMatch(i => i.Calls(dropAllHeldItemsMethodInfo))
+        };
+
+        private static readonly MethodInfo dropAllHeldItemsMethodInfo = typeof(PlayerControllerB).GetMethod("DropAllHeldItems", BindingFlags.Instance | BindingFlags.Public);
+        private static readonly MethodInfo dropAllButHeldItemMethodInfo = typeof(ShipTeleporterPatch).GetMethod("DropAllButHeldItem", BindingFlags.Static | BindingFlags.NonPublic);
+
+        [HarmonyTranspiler, HarmonyPatch("TeleportPlayerOutWithInverseTeleporter")]
+        public static IEnumerable<CodeInstruction> InverseTeleporterDropAllButHeldItem(IEnumerable<CodeInstruction> instructions) {
+            /*
+             * 	// playerControllerB.DropAllHeldItems();
+             *  ldloc.0
+             *  ldc.i4.1
+             *  ldc.i4.0
+             *  callvirt instance void GameNetcodeStuff.PlayerControllerB::DropAllHeldItems(bool, bool)
+             *  
+             *  becomes
+             *  
+             *  // ShipTeleporterPatch.DropAllButHeldItem(playerControllerB)
+             *  ldloc.0
+             *  callvirt void ShipTeleporterPatch::DropAllButHeldItem(PlayerControllerB)
+             */
+
+            CodeMatcher codeMatcher = new CodeMatcher(instructions);
+
+            codeMatcher.Start();
+            codeMatcher.MatchStartForward(inverseTeleporterPatchIlMatch);
+            codeMatcher.Advance(1);
+            codeMatcher.RemoveInstructionsWithOffsets(0, 2);
+            codeMatcher.Insert(new CodeInstruction(OpCodes.Callvirt, dropAllButHeldItemMethodInfo));
+
+            KindTeleportersBase.Log.LogInfo("Patched 'ShipTeleporterPatch.TeleportPlayerOutWithInverseTeleporter' :D");
+
+            return codeMatcher.Instructions();
+        }
+
+        [HarmonyTranspiler, HarmonyPatch("beamUpPlayer", MethodType.Enumerator)]
+        public static IEnumerable<CodeInstruction> TeleporterDropAllButHeldItem(IEnumerable<CodeInstruction> instructions) {
+            /*
+             * 	// playerControllerB.DropAllHeldItems();
+		     *  ldarg.0
+		     *  ldfld class GameNetcodeStuff.PlayerControllerB ShipTeleporter/'<beamUpPlayer>d__32'::'<playerToBeamUp>5__2'
+		     *  ldc.i4.1
+		     *  ldc.i4.0
+		     *  callvirt instance void GameNetcodeStuff.PlayerControllerB::DropAllHeldItems(bool, bool)
+             *  
+             *  becomes
+             *  
+             *  // ShipTeleporterPatch.DropAllButHeldItem(playerControllerB)
+             *  ldarg.0
+             *  ldfld class GameNetcodeStuff.PlayerControllerB ShipTeleporter/'<beamUpPlayer>d__32'::'<playerToBeamUp>5__2'
+             *  callvirt void ShipTeleporterPatch::DropAllButHeldItem(PlayerControllerB)
+             */
+
+            CodeMatcher codeMatcher = new CodeMatcher(instructions);
+
+            codeMatcher.End();
+            codeMatcher.MatchStartBackwards(teleporterPatchIlMatch);
+            codeMatcher.Advance(2);
+            codeMatcher.RemoveInstructionsWithOffsets(0, 2);
+            codeMatcher.Insert(new CodeInstruction(OpCodes.Callvirt, dropAllButHeldItemMethodInfo));
+
+            KindTeleportersBase.Log.LogInfo("Patched 'ShipTeleporterPatch.beamUpPlayer' :D");
+
+            return codeMatcher.Instructions();
+        }
+
+        private static void DropAllButHeldItem(PlayerControllerB player) {
+            bool itemsFall = true;
+            bool disconnecting = false;
+
+            for (int i = 0; i < player.ItemSlots.Length; i++) {
+                if (i == player.currentItemSlot) {
+                    continue;
+                }
+
+                GrabbableObject grabbableObject = player.ItemSlots[i];
+                if (!(grabbableObject != null)) {
+                    continue;
+                }
+                if (itemsFall) {
+                    grabbableObject.parentObject = null;
+                    grabbableObject.heldByPlayerOnServer = false;
+                    if (player.isInElevator) {
+                        grabbableObject.transform.SetParent(player.playersManager.elevatorTransform, worldPositionStays: true);
+                    } else {
+                        grabbableObject.transform.SetParent(player.playersManager.propsContainer, worldPositionStays: true);
+                    }
+                    player.SetItemInElevator(player.isInHangarShipRoom, player.isInElevator, grabbableObject);
+                    grabbableObject.EnablePhysics(enable: true);
+                    grabbableObject.EnableItemMeshes(enable: true);
+                    grabbableObject.transform.localScale = grabbableObject.originalScale;
+                    grabbableObject.isHeld = false;
+                    grabbableObject.isPocketed = false;
+                    grabbableObject.startFallingPosition = grabbableObject.transform.parent.InverseTransformPoint(grabbableObject.transform.position);
+                    grabbableObject.FallToGround(randomizePosition: true);
+                    grabbableObject.fallTime = Random.Range(-0.3f, 0.05f);
+                    if (player.IsOwner) {
+                        grabbableObject.DiscardItemOnClient();
+                    } else if (!grabbableObject.itemProperties.syncDiscardFunction) {
+                        grabbableObject.playerHeldBy = null;
+                    }
+                }
+                if (player.IsOwner && !disconnecting) {
+                    HUDManager.Instance.holdingTwoHandedItem.enabled = false;
+                    HUDManager.Instance.itemSlotIcons[i].enabled = false;
+                    HUDManager.Instance.ClearControlTips();
+                    player.activatingItem = false;
+                }
+                player.ItemSlots[i] = null;
+            }
+            player.twoHanded = player.ItemSlots[player.currentItemSlot].itemProperties.twoHanded;
+            player.carryWeight = Mathf.Clamp(1f - (player.ItemSlots[player.currentItemSlot].itemProperties.weight - 1f), 0f, 10f);
+        }
+    }
+}
---
+++ KindTeleporters/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace KindTeleporters
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class KindTeleportersBase : BaseUnityPlugin
+    {
+        public const string ModGUID = "stormytuna.KindTeleporters";
+        public const string ModName = "KindTeleporters";
+        public const string ModVersion = "1.0.0";
+
+        public static ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource(ModGUID);
+        public static KindTeleportersBase Instance;
+
+        private readonly Harmony harmony = new Harmony(ModGUID);
+
+        private void Awake() {
+            if (Instance is null) {
+                Instance = this;
+            }
+
+            Log.LogInfo("Kind Teleporters has awoken!");
+
+            harmony.PatchAll();
+        }
+    }
+}
